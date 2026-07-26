c6551e4f9827ac3c891acfe28fdb9b35cda23425
tksimeji <tksimeji@outlook.com>
2026-07-26 15:37:56 +0900

    feat: initialize BSS during kernel startup

--- src/boot.s
+++ src/boot.s
@@ -3,14 +3,25 @@
 .type _start, %function
 
 _start:
+    ldr x0, =_bss_start
+    ldr x1, =_bss_end
+
+.Lclear_bss:
+    cmp x0, x1
+    b.hs .Lbss_done
+
+    str xzr, [x0], #8
+    b .Lclear_bss
+
+.Lbss_done:
     ldr x0, =__stack_top
     mov sp, x0
 
     bl kernel_main
 
-1:
+.Lhang:
     wfe
-    b 1b
+    b .Lhang
 
 .section .bss.stack, "aw", @nobits
 .align 12
